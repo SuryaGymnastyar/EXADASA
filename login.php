2fe3d1b7f014e5e4c8acd81c9d0986d87d25186c
Rafly Saputra <966raflisaputra@gmail.com>
2026-04-27 12:35:53 +0700

    memperbaiki ui login

--- login.php
+++ login.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css" />
 </head>
 
 <body>
@@ -16,24 +20,25 @@
             <section class="form-group" style="margin-top: 16px;">
                 <section class="form-group-input">
                     <label for="nisn">Nisn</label>
-                    <input type="text" name="nisn" placeholder="Masukkan nisn..." id="nisn" class="input">
+                    <input type="text" name="nisn" placeholder="Masukkan nisn..." style="padding-right: 38px;" id="nisn" class="input">
+                    <i class="ph ph-user" style="position: absolute; bottom: 10px; right: 10px; font-size: 16px;"></i>
                 </section>
                 <section class="form-group-input">
                     <label for="password">Password</label>
-                    <input type="text" name="password" placeholder="Masukkan password..." id="password" class="input">
+                    <input type="text" name="password" placeholder="Masukkan password..." style="padding-right: 38px;" id="password" class="input">
+                    <i class="ph ph-eye-slash" style="position: absolute; bottom: 10px; right: 10px; font-size: 16px;"></i>
                 </section>
             </section>
             <section class="form-group-input checkbox">
                 <label for="remember" style="display: flex; align-items: center; gap: 5px;">
-                    <input type="checkbox" id="remember" name="remember"> <span style="font-size: 14px;">Remember me</span>
+                    <input type="checkbox" id="remember" name="remember"> <span style="font-size: 14px;">Remember
+                        me</span>
                 </label>
                 <a href="" style="color:#0000ff; font-size: 14px;">Lupa password?</a>
             </section>
             <section class="form-group-input">
                 <button type="submit" style="width: 100%;" class="btn-primary">Login</button>
             </section>
-            <p style="color:#000000a7; font-size: 14px; margin: 10px 0;">Belum punya akun? <a href=""
-                    style="color: #0000ff">Register</a></p>
         </form>
     </div>
 </body>
